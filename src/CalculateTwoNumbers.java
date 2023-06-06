2887c1583f6543991fabcfaa31893e1386aa8840
senakendi <ssenakendi@gmail.com>
2023-06-06 22:20:43 +0300

    added factorial method

--- src/CalculateTwoNumbers.java
+++ src/CalculateTwoNumbers.java
@@ -4,19 +4,39 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class CalculateTwoNumbers {
     public static void main(String[] args) {
+        //sumTwoNumber();
+        factorial();
 
-            Scanner reader = new Scanner(System.in);
+    }
+    public static void sumTwoNumber(){
 
-            System.out.print("Birinci Sayıyı Girin: ");
-            int sayi1 = reader.nextInt();
-            System.out.print("İkinci Sayıyı Girin: ");
-            int sayi2 = reader.nextInt();
+        Scanner reader = new Scanner(System.in);
 
-            int toplam = sayi1 + sayi2;
+        System.out.print("Birinci Sayıyı Girin: ");
+        int sayi1 = reader.nextInt();
+        System.out.print("İkinci Sayıyı Girin: ");
+        int sayi2 = reader.nextInt();
 
-            System.out.println("Sayıların Toplamı: " + toplam);
-        
+        int toplam = sayi1 + sayi2;
 
+        System.out.println("Sayıların Toplamı: " + toplam);
+    }
+    public static void factorial(){
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Bir Sayı Girin:");
 
+        int num = reader.nextInt();
+        int sayac=num;
+        long faktoriyel = 1;
+        while(sayac>1)
+        {
+            faktoriyel*=sayac;
+            sayac--;
+        }
+       /* for(int i = 1; i <= num; ++i)
+        {
+            faktoriyel *= i;
+        }*/
+        System.out.printf("%d Sayısının Faktöriyeli = %d \n", num, faktoriyel);
     }
 }
